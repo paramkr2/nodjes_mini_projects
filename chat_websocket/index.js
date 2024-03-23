61049bba84603e07aa4a2db5d65f9d7ecaf333fd
paramkr2 <param71kr@gmail.com>
2024-03-23 17:26:00 +0530

    with username

--- chat_websocket/index.js
+++ chat_websocket/index.js
@@ -15,35 +15,53 @@ app.get('/room',(req,res)=>{
 	res.sendFile(__dirname+'/room.html')
 })
 
+const users = new Map();
+
+// Handle WebSocket connections with Socket.IO
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    console.log(`User connected with socketid${socket.id}`);
+    // Handle setting username
+    socket.on('set username', (username,callback) => {
+        users.set(socket.id, { username });
+        console.log(`User ${username} connected with socket:${socket.id}`);
+		callback({ success: true });
+    });
 
     // Handle joining a room
-    socket.on('join room', (roomName,callback) => {
-        socket.join(roomName);
-        console.log(`User joined room: ${roomName}`);
+    socket.on('join room', (roomName , callback) => {
+        socket.join(roomName); 
+        console.log(`User ${users.get(socket.id).username} joined room: ${roomName} socket:${socket.id}`);
 		callback({ success: true });
     });
 
     // Handle leaving a room
-    socket.on('leave room', (roomName) => {
+    socket.on('leave room', (roomName ) => {
         socket.leave(roomName);
-        console.log(`User left room: ${roomName}`);
+        console.log(`User ${users.get(socket.id).username} left room: ${roomName}`);
     });
 
     // Handle chat messages
     socket.on('chat message', (data) => {
-        // Broadcast the message to all clients in the same room
-		console.log(data);
-        io.to(data.room).emit('chat message', data.message);
+        const { username } = users.get(socket.id);
+        io.to(data.room).emit('chat message', { username, message: data.message });
     });
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
+    // Handle disconnection
+   socket.on('disconnect', () => {
+    console.log(users);
+    if (users.has(socket.id)) {
+        const { username } = users.get(socket.id);
+        console.log(`User ${username} disconnected`);
+        users.delete(socket.id);
+    } else {
+        console.log(`User disconnected without setting a username`);
+    }
 });
 
+});
 
-server.listen(3000, () => {
-    console.log('Server listening on port 3000');
+// Start the HTTP server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
